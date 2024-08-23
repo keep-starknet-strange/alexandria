cdc4020eff1e49c03a7940f2c57b3525efb3299e
Milan Cermak <milan.cermak@gmail.com>
2024-08-23 15:54:36 +0200

    feat: derive Add, Sub, Mul, Div

---
+++ packages/macros/src/num_traits.rs
@@ -0,0 +1,168 @@
+use cairo_lang_macro::{derive_macro, ProcMacroResult, TokenStream};
+use cairo_lang_parser::utils::SimpleParserDatabase;
+use cairo_lang_syntax::node::kind::SyntaxKind::{
+    Member, OptionWrappedGenericParamListEmpty, TerminalStruct, TokenIdentifier,
+    WrappedGenericParamList,
+};
+
+struct StructInfo {
+    name: String,
+    generic_params: Option<Vec<String>>,
+    members: Vec<String>,
+}
+
+struct OpInfo {
+    trait_name: String,
+    fn_name: String,
+    operator: String,
+}
+
+fn parse_tokenstream_struct(token_stream: TokenStream) -> StructInfo {
+    let db = SimpleParserDatabase::default();
+    let (parsed, _diag) = db.parse_virtual_with_diagnostics(token_stream);
+    let mut nodes = parsed.descendants(&db);
+
+    // find struct name - the next TokenIdentifier after TeminalStruct
+    let mut struct_name = String::new();
+    while let Some(node) = nodes.next() {
+        if node.kind(&db) == TerminalStruct {
+            struct_name = nodes
+                .find(|node| node.kind(&db) == TokenIdentifier)
+                .unwrap()
+                .get_text(&db);
+            break;
+        }
+    }
+
+    // collect generic params or skip if there aren't any
+    let mut generic_params: Option<Vec<String>> = None;
+    while let Some(node) = nodes.next() {
+        match node.kind(&db) {
+            WrappedGenericParamList => {
+                let params = node
+                    .descendants(&db)
+                    .filter(|node| node.kind(&db) == TokenIdentifier)
+                    .map(|node| node.get_text(&db))
+                    .collect();
+                generic_params = Some(params);
+                break;
+            }
+            OptionWrappedGenericParamListEmpty => {
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    // collect struct members - all TokenIdentifier nodes after each Member
+    let mut members = Vec::new();
+    while let Some(node) = nodes.next() {
+        if node.kind(&db) == Member {
+            let member = node
+                .descendants(&db)
+                .find(|node| node.kind(&db) == TokenIdentifier)
+                .map(|node| node.get_text(&db))
+                .unwrap();
+            members.push(member);
+        }
+    }
+
+    StructInfo {
+        name: struct_name,
+        generic_params,
+        members,
+    }
+}
+
+fn generate_trait_impl(op_info: &OpInfo, s: &StructInfo) -> String {
+    let generic_params = s
+        .generic_params
+        .as_ref()
+        .map_or(String::new(), |params| format!("<{}>", params.join(", ")));
+
+    let trait_bounds = s.generic_params.as_ref().map_or_else(
+        || String::new(),
+        |params| {
+            let bounds = params
+                .iter()
+                .flat_map(|param| {
+                    vec![
+                        format!("+core::traits::{}<{}>", op_info.trait_name, param),
+                        format!("+core::traits::Drop<{}>", param),
+                    ]
+                })
+                .collect::<Vec<_>>()
+                .join(",\n");
+            format!("<{},\n{}>", params.join(", "), bounds)
+        },
+    );
+
+    let members_op = s
+        .members
+        .iter()
+        .map(|member| format!("{0}: lhs.{0} {1} rhs.{0}", member, op_info.operator))
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    format!(
+        "\n
+impl {0}{1}{2}
+of core::traits::{1}<{0}{3}> {{
+    fn {4}(lhs: {0}{3}, rhs: {0}{3}) -> {0}{3} {{
+        {0} {{ {5} }}
+    }}
+}}\n",
+        s.name, op_info.trait_name, trait_bounds, generic_params, op_info.fn_name, members_op
+    )
+}
+
+// TODO: add macro docs so that they show in VSCode
+
+#[derive_macro]
+pub fn add(token_stream: TokenStream) -> ProcMacroResult {
+    let op = OpInfo {
+        trait_name: "Add".to_string(),
+        fn_name: "add".to_string(),
+        operator: "+".to_string(),
+    };
+
+    let s = parse_tokenstream_struct(token_stream);
+
+    ProcMacroResult::new(TokenStream::new(generate_trait_impl(&op, &s)))
+}
+
+#[derive_macro]
+pub fn sub(token_stream: TokenStream) -> ProcMacroResult {
+    let op = OpInfo {
+        trait_name: "Sub".to_string(),
+        fn_name: "sub".to_string(),
+        operator: "-".to_string(),
+    };
+    let s = parse_tokenstream_struct(token_stream);
+
+    ProcMacroResult::new(TokenStream::new(generate_trait_impl(&op, &s)))
+}
+
+#[derive_macro]
+pub fn mul(token_stream: TokenStream) -> ProcMacroResult {
+    let op = OpInfo {
+        trait_name: "Mul".to_string(),
+        fn_name: "mul".to_string(),
+        operator: "*".to_string(),
+    };
+    let s = parse_tokenstream_struct(token_stream);
+
+    ProcMacroResult::new(TokenStream::new(generate_trait_impl(&op, &s)))
+}
+
+#[derive_macro]
+pub fn div(token_stream: TokenStream) -> ProcMacroResult {
+    let op = OpInfo {
+        trait_name: "Div".to_string(),
+        fn_name: "div".to_string(),
+        operator: "/".to_string(),
+    };
+    let s = parse_tokenstream_struct(token_stream);
+
+    ProcMacroResult::new(TokenStream::new(generate_trait_impl(&op, &s)))
+}
--- packages/macros_tests/src/lib.cairo
+++ packages/macros_tests/src/lib.cairo
@@ -1,15 +1,5 @@
-const TWO_TEN: u128 = pow!(2, 10);
+#[cfg(test)]
+mod test_pow;
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn test_pow_macro() {
-        assert_eq!(super::TWO_TEN, 1024);
-        assert_eq!(pow!(10, 2), 100);
-        assert_eq!(pow!(20, 30), 1073741824000000000000000000000000000000_felt252);
-        assert_eq!(
-            pow!(2, 255),
-            57896044618658097711785492504343953926634992332820282019728792003956564819968_u256
-        );
-    }
-}
+mod test_num_traits;
